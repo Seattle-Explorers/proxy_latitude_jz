e7a6e85dbda135db5c8215d8b871d79e72627a68
Janet Zheng <janet.zhengg@gmail.com>
2020-07-23 12:37:30 -0700

    added set for review comp

--- index.js
+++ index.js
@@ -16,19 +16,27 @@ const descOptions = {
   changeOrigin: true,
 };
 
-const app = express();
+const revOptions = {
+  target: process.env.REV_HOST || 'localhost: 3002',
+  changeOrigin: true,
+};
+
 const proxyPath = path.join(__dirname);
 const proxyRes = createProxyMiddleware(resOptions);
 const proxyDesc = createProxyMiddleware(descOptions);
+const proxyRev = createProxyMiddleware(revOptions);
 
+const app = express();
 app.use('/:id', express.static(proxyPath));
 
-app.get('/:id/reservation/style.css', proxyRes);
 app.get('/:id/reservation/reservationBundle.js', proxyRes);
 app.get('/api/reservation/:id', proxyRes);
 
 app.get('/description/main.js', proxyDesc);
 app.get('/api/description/:id', proxyDesc);
+
+app.get('/review/bundle.js', proxyRev);
+app.get('/api/description/:id', proxyRev);
 
 app.listen(port, () => {
   Console.log(`proxy listening on port ${port}`);
